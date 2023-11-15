53035ee7c9636f9a6639687778221da10829dbb7
SurgeArrester <cameron.h@rgreaves.me.uk>
2023-11-15 13:12:01 +0100

    Added routing for other html templates

--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -4,3 +4,31 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contribute')
+def contribute():
+    return render_template('contribute.html')
+
+@app.route('/direct_table_test')
+def direct_table_test():
+    return render_template('direct_table_test.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+@app.route('/paperlist')
+def paperlist():
+    return render_template('paperlist.html')
+
+@app.route('/papers')
+def papers():
+    return render_template('papers.html')
+
+@app.route('/abinintsitepage')
+def abinintsitepage():
+    return render_template('abinintsitepage.html')
